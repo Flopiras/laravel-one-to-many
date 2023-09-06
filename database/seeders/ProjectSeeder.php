3742040563f0ce2787516e41a235e6ae4ba92dc2
Flopiras <Flopiras@yahoo.it>
2023-09-06 17:55:29 +0200

    add type_id in project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -16,10 +18,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(Generator $faker): void
     {
+        $type_ids = Type::pluck('id')->toArray();
+
         Storage::makeDirectory('project_images');
         for ($i = 0; $i < 10; $i++) {
             $project = new Project();
 
+            $project->type_id = Arr::random($type_ids);
             $project->title = $faker->text(20);
             $project->slug = Str::slug($project->title, '-');
             $project->content = $faker->paragraphs(15, true);
